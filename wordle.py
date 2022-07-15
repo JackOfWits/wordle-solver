52ae126876733431165a9dd2c56f6ac2dbd71607
jack.of.wits <noah.jackowitz@jsf.mil>
2022-07-14 21:46:09 -0700

    remove unused code

--- wordle.py
+++ wordle.py
@@ -15,37 +15,6 @@ with open(os.path.join(os.path.dirname(__file__), 'words.txt'), 'r') as fid:
     WORDS = fid.read().strip().split()
 
 
-class Wordle:
-
-    def __init__(self, seed=None):
-        seed = re.compile(seed or '')
-        options = [word for word in WORDS if seed.search(word)]
-        if not options:
-            raise ValueError(f'No words match `{seed!s}`')
-        self.solution = random.choice([word for word in WORDS if seed.search(word)])
-        self.candidates = WORDS.copy()
-        print(f'WORDLE: SOLUTION = {self.solution}')
-
-    def main(self):
-        for nguess in range(NGUESSES):
-            # choose a remaining candidate and stick with it
-            guess = random.choice(self.candidates)
-            # TODO: look at all the options and choose the most promising one
-            # nopts = len(self.candidates)
-            # for candidate in self.candidates:
-            #     ...
-            print(f'WORDLE: GUESS[{nguess + 1}] = {len(self.candidates):04} {guess} {compare(guess, self.solution)}')
-            # if the guess is correct, we win!
-            if guess == self.solution:
-                print(f'WORDLE: SOLVED IN {nguess + 1} GUESSES')
-                return 0
-            # else reduce the candidates and keep going
-            self.candidates = filter(guess, self.solution, self.candidates)
-
-        # after all guesses are exhausted, we exit with an error code
-        print(f'WORDLE: FAILED')
-
-
 def filter(guess: str, answer: str, candidates: list[str] = WORDS) -> list[str]:
     """Filter candidates by the result of a guess."""
     pattern = compare(guess, answer)
@@ -85,11 +54,3 @@ def score(guess: str, candidates: list[str] = WORDS) -> float:
         - (guess in candidates)  # 0 more guesses if you're right!
         for candidate in candidates
     ) / len(candidates)
-
-
-if __name__ == '__main__':
-    with open('scores.txt', 'w') as fid:
-        for word in tqdm(WORDS):
-            value = score(word)
-            fid.write(f'{word}: {value},')
-            print('\r', word, value)
